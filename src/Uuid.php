5754259b6d0409e9b024a75e8ff750ffefa0539c
Marcel Hernandez <e6990620@gmail.com>
2017-12-13 22:13:24 +0100

    internal refactor of Uuid: hold uuid in textual format only

--- src/Uuid.php
+++ src/Uuid.php
@@ -22,30 +22,64 @@ class Uuid
     const TEXTUAL_FORMAT = '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i';
 
     /**
-     * Ordered sequence of 16 bytes.
-     *
-     * @example 0x96aaab697b764461b008cbb9cfcb6fdf
-     *
-     * @var string
-     */
-    private $raw;
-
-    /**
-     * Textual UUID representation of the $raw byte sequence.
+     * Textual representation of the UUID byte sequence.
      *
      * @example '96aaab69-7b76-4461-b008-cbb9cfcb6fdf'
      *
      * @var string
      */
-    private $textual;
+    private $uuid;
 
     /**
-     * Direct usage of the constructor is only allowed inside the class.
+     * @throws \InvalidArgumentException If $text is not a valid Uuid in string format.
      */
-    private function __construct(string $raw, string $textual)
+    private function __construct(string $text)
     {
-        $this->raw = $raw;
-        $this->textual = \strtolower($textual);
+        if (false === self::isUuid($text)) {
+            throw new \InvalidArgumentException('$text is not a valid Uuid. Got: ' . $text);
+        }
+
+        $this->uuid = \strtolower($text);
+    }
+
+    /**
+     * Factory method to create a new Uuid instance from a valid Uuid in string form.
+     *
+     * As opposed to the fromBytes() method, this one is meant to
+     * be used by the end user of the library.
+     *
+     * @throws \InvalidArgumentException If $text is not a valid Uuid in string format.
+     */
+    public static function fromString(string $text): Uuid
+    {
+        return new self($text);
+    }
+
+    /**
+     * Factory method to create a new Uuid instance from a raw byte sequence.
+     *
+     * Most of the time this method should be used by UuidGenerator
+     * implementations, not the end user of the library.
+     *
+     * @throws \InvalidArgumentException If $bytes is not exactly 16 bytes long.
+     */
+    public static function fromBytes(string $bytes): Uuid
+    {
+        if (16 !== \strlen($bytes)) {
+            throw new \InvalidArgumentException('Length of $bytes for new Uuid is not 16. Got: 0x' . \bin2hex($bytes));
+        }
+
+        return new self(self::bin2str($bytes));
+    }
+
+    /**
+     * Factory method to create a new 'Nil' Uuid instance.
+     *
+     * @throws \InvalidArgumentException
+     */
+    public static function nil(): Uuid
+    {
+        return new self(self::NIL);
     }
 
     /**
@@ -61,7 +95,7 @@ class Uuid
      */
     public function asBytes(): string
     {
-        return $this->raw;
+        return self::str2bin($this->uuid);
     }
 
     /**
@@ -69,49 +103,7 @@ class Uuid
      */
     public function asString(): string
     {
-        return $this->textual;
-    }
-
-    /**
-     * Factory to create a new Uuid instance from a raw byte sequence.
-     *
-     * Most of the time this method should be used by UuidGenerator
-     * implementations, not the end user of the library.
-     *
-     * @throws \InvalidArgumentException If $bytes is not exactly 16 bytes long.
-     */
-    public static function fromBytes(string $bytes): Uuid
-    {
-        if (16 !== \strlen($bytes)) {
-            throw new \InvalidArgumentException('Length of $bytes for new Uuid is not 16. Got: 0x' . \bin2hex($bytes));
-        }
-
-        return new self($bytes, self::bin2str($bytes));
-    }
-
-    /**
-     * Factory to create a new Uuid instance from a valid Uuid in string form.
-     *
-     * As opposed to the fromBytes() method, this one is meant to
-     * be used by the end user of the library.
-     *
-     * @throws \InvalidArgumentException If $text is not actually a valid Uuid.
-     */
-    public static function fromString(string $text): Uuid
-    {
-        if (false === self::isUuid($text)) {
-            throw new \InvalidArgumentException('$text is not a valid Uuid. Got: ' . $text);
-        }
-
-        return new self(self::str2bin($text), $text);
-    }
-
-    /**
-     * Factory to create a new 'Nil' Uuid instance.
-     */
-    public static function nil(): Uuid
-    {
-        return self::fromString(self::NIL);
+        return $this->uuid;
     }
 
     /**
