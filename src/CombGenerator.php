2e09610db7de6c6d07821761b6642e927439722c
Marcel Hernandez <e6990620@gmail.com>
2018-01-13 00:54:01 +0100

    generalized Comb algorithm into CustomCombGenerator

--- src/CombGenerator.php
+++ src/CombGenerator.php
@@ -7,18 +7,8 @@ namespace UMA\Uuid;
 /**
  * @see http://www.informit.com/articles/article.aspx?p=25862
  */
-class CombGenerator implements UuidGenerator
+class CombGenerator extends CustomCombGenerator
 {
-    /**
-     * @var int
-     */
-    private $exponent;
-
-    /**
-     * @var Version4Generator
-     */
-    private $v4;
-
     /**
      * @param int $granularity Precision of the timestamps, ranging from second up to microsecond.
      *
@@ -26,81 +16,10 @@ class CombGenerator implements UuidGenerator
      */
     public function __construct(int $granularity = 6)
     {
-        if (!\in_array($granularity, \range(0, 6), true)) {
-            throw new \InvalidArgumentException('$granularity must be in the [0, 6] range. Got: ' . $granularity);
-        }
-
-        $this->exponent = 10 ** $granularity;
-        $this->v4 = new Version4Generator;
-    }
-
-    /**
-     * @throws \Exception If $v4->generate() fails due to exhausted entropy.
-     */
-    public function generate(string $name = null): Uuid
-    {
-        $head = $this->procrust($this->timestamp());
-        $tail = \substr($this->v4->generate()->asBytes(), -10);
-
-        return Uuid::fromBytes($head . $tail);
-    }
-
-    /**
-     * Returns the exact date on which the 48 most significant bits of
-     * the UUIDs will overflow for the chosen $granularity.
-     *
-     * The higher the granularity the better is the output of the
-     * generator, but the overflow date also looms sooner.
-     */
-    public function getOverflowDate(): \DateTimeImmutable
-    {
-        $fullTimestampLength = \strlen($this->timestamp());
-        $choppedDigits = $fullTimestampLength < 12 ? 0 : $fullTimestampLength - 12;
-        $maxTimestamp = (int)(\hexdec(\str_pad('', 12 + $choppedDigits, 'f'))/$this->exponent);
-
-        return new \DateTimeImmutable("@$maxTimestamp UTC");
-    }
-
-    /**
-     * Returns $timestamp "procrusted" to 6 bytes.
-     *
-     * If the timestamp is smaller than 6 bytes, leading 0 bits are appended.
-     * If the timestamp is larger than 6 bytes, its least significant bits are chopped off.
-     *
-     * The returned string is raw binary (each character encodes 8 bits)
-     * and has always the same size -- 6 bytes.
-     *
-     * @example '59b7d71f'      => 0x000059b7d71f
-     * @example '3812e6738'     => 0x0003812e6738
-     * @example '230bd00838'    => 0x00230bd00838
-     * @example '15e76205236'   => 0x015e76205236
-     * @example 'db09d433621'   => 0x0db09d433621
-     * @example '88e624a01d4c'  => 0x88e624a01d4c
-     * @example '558fd6e4124fb' => 0x558fd6e4124f
-     */
-    private function procrust(string $timestamp): string
-    {
-        return \pack('H12', \str_pad(\substr($timestamp, 0, 12), 12, '0', STR_PAD_LEFT));
-    }
-
-    /**
-     * Returns the current unix timestamp as a hex-encoded string (that is, each character
-     * encodes 4 bits) with variable precision, ranging from second to microsecond.
-     *
-     * The length of the string varies depending on the $granularity chosen. This is how
-     * the exact same reading from microtime() looks like for all 7 possible granularity
-     * levels (0 through 6):
-     *
-     * @example '59b7d71f'
-     * @example '3812e6738'
-     * @example '230bd00838'
-     * @example '15e76205236'
-     * @example 'db09d433621'
-     * @example '88e624a01d4c'
-     * @example '558fd6e4124fb'
-     */
-    private function timestamp(): string
-    {
-        return \dechex((int)(\microtime(true) * $this->exponent));
+        parent::__construct(
+            new Version4Generator,
+            new \DateTimeImmutable('1970-01-01 00:00:00 UTC'),
+            6, $granularity
+        );
     }
 }
---
+++ src/CustomCombGenerator.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UMA\Uuid;
+
+class CustomCombGenerator implements UuidGenerator
+{
+    /**
+     * @var UuidGenerator
+     */
+    private $generator;
+
+    /**
+     * @var int
+     */
+    private $exponent;
+
+    /**
+     * @var int
+     */
+    private $epoch;
+
+    /**
+     * @var int
+     */
+    private $span;
+
+    /**
+     * @param UuidGenerator      $generator
+     * @param \DateTimeImmutable $epoch
+     * @param int                $span
+     * @param int                $granularity
+     *
+     * @throws \InvalidArgumentException When $epoch is a date in the future.
+     * @throws \InvalidArgumentException When $span is outside the [1, 16] range.
+     * @throws \InvalidArgumentException When $granularity is outside the [0, 6] range.
+     */
+    public function __construct(UuidGenerator $generator, \DateTimeImmutable $epoch, int $span, int $granularity)
+    {
+        if (new \DateTimeImmutable('now') < $epoch) {
+            throw new \InvalidArgumentException('$epoch must be in the past. Got timestamp: ' . $epoch->getTimestamp());
+        }
+
+        if (!\in_array($span, \range(1, 16), true)) {
+            throw new \InvalidArgumentException('$span must be in the [1, 16] range. Got: ' . $span);
+        }
+
+        if (!\in_array($granularity, \range(0, 6), true)) {
+            throw new \InvalidArgumentException('$granularity must be in the [0, 6] range. Got: ' . $granularity);
+        }
+
+        $this->generator = $generator;
+        $this->exponent = 10 ** $granularity;
+        $this->epoch = $epoch->getTimestamp() * $this->exponent;
+        $this->span = 2 * $span;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function generate(string $name = null): Uuid
+    {
+        $head = $this->procrust($this->timestamp());
+        $tail = \substr($this->generator->generate()->asBytes(), -16 + ($this->span / 2));
+
+        return Uuid::fromBytes($head . $tail);
+    }
+
+    /**
+     * Returns the exact date on which the 48 most significant bits of
+     * the UUIDs will overflow for the chosen $granularity.
+     *
+     * The higher the granularity the better is the output of the
+     * generator, but the overflow date also looms sooner.
+     */
+    public function getOverflowDate(): \DateTimeImmutable
+    {
+        $fullTimestampLength = \strlen($this->timestamp());
+        $choppedDigits = $fullTimestampLength < $this->span ? 0 : $fullTimestampLength - $this->span;
+        $maxTimestamp = (int)(\hexdec(\str_pad('', $this->span + $choppedDigits, 'f'))/$this->exponent);
+
+        return new \DateTimeImmutable("@$maxTimestamp UTC");
+    }
+
+    /**
+     * Returns $timestamp "procrusted" to 6 bytes.
+     *
+     * If the timestamp is smaller than 6 bytes, leading 0 bits are appended.
+     * If the timestamp is larger than 6 bytes, its least significant bits are chopped off.
+     *
+     * The returned string is raw binary (each character encodes 8 bits)
+     * and has always the same size -- 6 bytes.
+     *
+     * @example '59b7d71f'      => 0x000059b7d71f
+     * @example '3812e6738'     => 0x0003812e6738
+     * @example '230bd00838'    => 0x00230bd00838
+     * @example '15e76205236'   => 0x015e76205236
+     * @example 'db09d433621'   => 0x0db09d433621
+     * @example '88e624a01d4c'  => 0x88e624a01d4c
+     * @example '558fd6e4124fb' => 0x558fd6e4124f
+     */
+    private function procrust(string $timestamp): string
+    {
+        return \pack("H{$this->span}", \str_pad(\substr($timestamp, 0, $this->span), $this->span, '0', STR_PAD_LEFT));
+    }
+
+    /**
+     * Returns the current unix timestamp as a hex-encoded string (that is, each character
+     * encodes 4 bits) with variable precision, ranging from second to microsecond.
+     *
+     * The length of the string varies depending on the $granularity chosen. This is how
+     * the exact same reading from microtime() looks like for all 7 possible granularity
+     * levels (0 through 6):
+     *
+     * @example '59b7d71f'
+     * @example '3812e6738'
+     * @example '230bd00838'
+     * @example '15e76205236'
+     * @example 'db09d433621'
+     * @example '88e624a01d4c'
+     * @example '558fd6e4124fb'
+     */
+    private function timestamp(): string
+    {
+        return \dechex((int)(\microtime(true) * $this->exponent) - $this->epoch);
+    }
+}
